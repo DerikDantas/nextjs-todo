09d13b6be949fa705f65c9a05c334812cbaf7bfa
derikdantas <derikdantas@hotmail.com>
2024-05-21 22:15:52 -0300

    feat: adding react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ReactQueryClientProvider } from '@/components/react-query-provider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ToastContainer } from 'react-toastify';
@@ -23,10 +24,12 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={inter.className}>
-        <AuthProvider>
-          <div className="max-w-3xl mx-auto p-4">{children}</div>
-          <ToastContainer />
-        </AuthProvider>
+        <ReactQueryClientProvider>
+          <AuthProvider>
+            <div className="max-w-3xl mx-auto p-4">{children}</div>
+            <ToastContainer />
+          </AuthProvider>
+        </ReactQueryClientProvider>
       </body>
     </html>
   );
